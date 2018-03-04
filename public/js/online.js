a61f4ba90bc435b1e0485971f60c2309106e9b69
Toofifty <alex@matho.me>
2018-03-04 23:54:37 +1100

    Implement multiplayer

--- public/js/online.js
+++ public/js/online.js
@@ -4,21 +4,22 @@
 
 const joinOnline = (key) => {
     const socket = io()
-    console.log('joining lobby', key)
     socket.emit('join lobby', key)
-    socket.on('joined lobby', res => {
+    socket.once('join lobby response', res => {
         if (res === 'lobby full') {
             console.error('lobby full')
         } else if (res === 'not exists') {
             console.error('lobby doesn\'t exist')
         } else {
+            console.log('joined lobby')
             initOnlineGame(key, socket, res.player, res.lobby.gameSize, res.lobby.playerCount)
         }
     })
 }
 
 const initOnlineGame = (key, socket, playerID, gameSize, playerCount) => {
-    socket.on('begin game', () => {
+    socket.once('begin game', () => {
+        console.log('begin game')
         runOnlineGame(key, socket, playerID, gameSize, playerCount)
     })
     if (gameSize % 2 || gameSize % 2) {
@@ -29,28 +30,28 @@ const initOnlineGame = (key, socket, playerID, gameSize, playerCount) => {
     $('#app').append(board)
     board.css({ width: board.height() + 'px' })
     $('#app').append(initScoreboard(playerCount))
-    $('#app').append($('<div class="toggle-posts">Toggle posts</div>'))
+    $('#app').append($('<div class="toggle-posts">You are player ' + playerID + '<br>Toggle posts</div>'))
 
     $('.toggle-posts').click(function () {
         $('#app').toggleClass('show-posts')
     })
 }
 
-const runOnlineGame = (key, socket, playerID, gameSize, playerCount) => {
+const runOnlineGame = (gameKey, socket, playerID, gameSize, playerCount) => {
     let currentPlayer = 0
 
     socket.on('set player', (id) => {
         currentPlayer = id
     })
-    socket.on('place', ({ x, y, playerID }) => {
-        place(x, y, playerID)
+    socket.on('place', ({ key, x, y, playerID }) => {
+        if (key === gameKey) place(x, y, playerID)
     })
 
     $('.gate-container').click(function () {
         if (currentPlayer === playerID) {
             const id = $(this).attr('id')
             const coords = id.replace('gate-', '').split('-').map(i => parseInt(i))
-            socket.emit('place', { key, ...coords, playerID })
+            socket.emit('place', { key: gameKey, x: coords[0], y: coords[1], playerID })
         }
     })
 
--- server.js
+++ server.js
@@ -28,13 +28,13 @@ io.on('connection', function (socket) {
         this.emit('lobby confirm', key)
     })
     socket.on('join lobby', (key) => {
-        console.log(socket, 'trying to join lobby', key)
+        console.log(socket.id, 'trying to join lobby', key)
         if (!lobbies[key]) {
             this.emit('join lobby response', 'not exists')
 
         } else if (lobbies[key].join(socket.id)) {
             this.emit('join lobby response', {
-                player: lobbies[key].connectedPlayers - 1,
+                player: lobbies[key].connectedPlayers.length - 1,
                 lobby: lobbies[key]
             })
             if (lobbies[key].full()) {
@@ -45,8 +45,16 @@ io.on('connection', function (socket) {
             this.emit('join lobby response', 'lobby full')
         }
     })
-    socket.on('place', ({ key, x, y, playerID }) => {
-        lobbies[key].place(x, y, playerID)
+    socket.on('place', function ({ key, x, y, playerID }) {
+        // if no paddock was captured, change player
+        let changePlayer = !lobbies[key].place(x, y, playerID)
+        this.emit('place', { key, x, y, playerID })
+        this.broadcast.emit('place', { key, x, y, playerID })
+        if (changePlayer) {
+            let nextPlayer = lobbies[key].nextPlayer(playerID)
+            this.emit('set player', nextPlayer)
+            this.broadcast.emit('set player', nextPlayer)
+        }
     })
 })
 
@@ -54,6 +62,18 @@ function Lobby (gameSize, playerCount) {
     this.gameSize = gameSize
     this.playerCount = playerCount
     this.connectedPlayers = []
+    this.board = (() => {
+        let temp = []
+        for (let j = 0; j < gameSize; j++) {
+            let line = []
+            for (let i = 0; i < gameSize; i++) {
+                let type = (i + j) % 2 === 0 ? 'bltr' : 'tlbr'
+                line.push({ type, taken: false })
+            }
+            temp.push(line)
+        }
+        return temp
+    })()
     this.join = (socketID) => {
         if (!this.full()) {
             this.connectedPlayers.push(socketID)
@@ -64,7 +84,54 @@ function Lobby (gameSize, playerCount) {
     this.full = () => {
         return this.connectedPlayers.length === this.playerCount
     }
-    this.place = () => {
-        
+    this.nextPlayer = (playerID) => {
+        return (playerID + 1) % this.playerCount
+    }
+    this.getGate = (x, y) => {
+        // check bounds
+        if (
+            y < 0 || y >= this.board.length ||
+            x < 0 || x >= this.board[y].length
+        ) {
+            return { taken: false }
+        }
+        return this.board[y][x]
+    }
+    this.place = (x, y, playerID) => {
+        this.getGate(x, y).taken = true
+        let setTop = []
+        let setBottom = []
+        // see public/js/game.js @checkPaddock
+        // for an explanation
+        if (this.getGate(x, y).type === 'tlbr') {
+            setTop = [
+                this.getGate(x, y),
+                this.getGate(x, y - 1),
+                this.getGate(x + 1, y - 1),
+                this.getGate(x + 1, y)
+            ]
+            setBottom = [
+                this.getGate(x, y),
+                this.getGate(x, y + 1),
+                this.getGate(x - 1, y + 1),
+                this.getGate(x - 1, y)
+            ]
+        } else {
+            setTop = [
+                this.getGate(x, y),
+                this.getGate(x - 1, y),
+                this.getGate(x - 1, y - 1),
+                this.getGate(x, y - 1)
+            ]
+            setBottom = [
+                this.getGate(x, y),
+                this.getGate(x + 1, y),
+                this.getGate(x + 1, y + 1),
+                this.getGate(x, y + 1)
+            ]
+        }
+        let topFilled = (setTop.filter(v => v.taken).length === 4)
+        let bottomFilled = (setBottom.filter(v => v.taken).length === 4)
+        return topFilled || bottomFilled
     }
 }
